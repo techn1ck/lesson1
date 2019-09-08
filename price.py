adc90626bf0610371883269d029b0a05450b8cec
Nikita Yakovlev <nikita.uventa@gmail.com>
2019-09-08 14:09:01 +0300

    Проверка работы с удаленным репозиторием

--- price.py
+++ price.py
@@ -1,10 +1,10 @@
+"""
 def get_summ (one, two, delimiter='&'):
     result = str(one) + str(delimiter) + str(two)
     return result.upper()
 
 print(get_summ("Learn", "Python", " - "))
-
-
+"""
 
 def format_price (price):
     try:
